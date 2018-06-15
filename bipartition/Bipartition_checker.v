34750a6824195078865184266d9c5e24697f6d07
Alexander Boll <boll@informatik.hu-berlin.de>
2018-06-15 16:00:07 +0200

    autocommit 2018-06-15-16:00:07

--- bipartition/Bipartition_checker.v
+++ bipartition/Bipartition_checker.v
@@ -505,20 +505,21 @@ Proof.
     intros. apply (leader_same_lemma) ; auto.
     clear leader_same_lemma.
     assert (forall x : Component, v x ->
-        leader_same (leader_i (construct_checker_input x)) (neighbor_leader_distance (construct_checker_input x)) = true /\
-        ((is_leader x = true /\ is_parent x = true /\ distance_i (construct_checker_input x) = 0) \/
-        (is_leader x <> true /\ a (A_ends x (parent_i (construct_checker_input x))) /\
+        ((x = leader_i (construct_checker_input x) /\ x = parent_i (construct_checker_input x) /\ distance_i (construct_checker_input x) = 0) \/
+        (x <> leader_i (construct_checker_input x) /\ a (A_ends x (parent_i (construct_checker_input x))) /\
           (distance_i (construct_checker_input x) = distance_i (construct_checker_input (parent_i (construct_checker_input x))) + 1)))).
     intros.
     specialize (H x0) ; intuition.
-    apply andb_true_iff in H1. destruct H1. auto.
     apply andb_true_iff in H1. destruct H1.
     apply orb_true_iff in H1. destruct H1.
     left.
     apply andb_true_iff in H1. destruct H1.
     apply andb_true_iff in H1. destruct H1.
     apply beq_nat_true in H2.
-    intuition.
+    unfold is_leader in H1. unfold is_parent in H3.
+    destruct (V_eq_dec x0 (leader_i (construct_checker_input x0))) ; subst ; intuition.
+    destruct (V_eq_dec x0 (parent_i (construct_checker_input x0))) ; subst ; intuition.
+    inversion H3. inversion H1. inversion H1.
     right.
     apply andb_true_iff in H1. destruct H1.
     apply andb_true_iff in H1. destruct H1.
@@ -526,22 +527,96 @@ Proof.
     apply negb_true_iff in H1.
     apply is_in_correct in H3.
     apply neighborhood_correct in H3.
-    intuition. rewrite H4 in H1. inversion H1.
+    unfold is_leader in H1.
+    destruct (V_eq_dec x0 (leader_i (construct_checker_input x0))) ; subst ; intuition.
     clear H.
-    
+    generalize H0 lsl Hx. generalize x.
+    clear H0 lsl Hx x.
+
     induction c ; simpl in * ; subst ; intuition.
     + inversion Hx. subst.
-      specialize (H0 x).
+      specialize (H0 x0).
       intuition.
-      unfold is_leader in H1.
-      destruct (V_eq_dec x (leader_i (construct_checker_input x))) ; subst ; intuition.
-      rewrite <- e. apply In_single.
-      inversion H1.
-    + admit.
-    + assert (H0' := H0). assert (H0'' := H0).
+      rewrite <- H. apply In_single.
+      inversion H0.
+    + destruct (V_eq_dec y (leader_i (construct_checker_input x0))).
+      subst. apply In_left. apply In_single.
+      apply In_right.
+      inversion Hx.
+      inversion H1 ; subst ; intuition.
+      rewrite (lsl x0 x) ; auto.
+      apply (H x) ; auto ; intuition ; clear H.
+      assert (H0' := H0). assert (H0'' := H0).
+      specialize (H0 x1). specialize (H0' x0). specialize (H0'' x).
+      assert (V_union (V_single x0) v x1). apply In_right. auto.
+      assert (V_union (V_single x0) v x). apply In_right. auto.
+      intuition ; subst ; intuition ; right ; intuition.
+      inversion H5 ; subst ; inversion H9 ; subst ; intuition.
+      rewrite (lsl x x0) in * ; auto ; subst ; intuition.
+      inversion H0 ; auto. inversion H8 ; subst ; intuition.
+      rewrite <- H17 in *.
+      rewrite H7 in H9. clear H7.
+      rewrite Nat.add_comm in H9. simpl in H9.
+      rewrite Nat.add_comm in H9. simpl in H9.
+      induction (distance_i (construct_checker_input (leader_i (construct_checker_input x0)))) ; simpl in *.
+      rewrite Nat.add_comm in H14. simpl in H14. inversion H14.
+      inversion H12.
+      inversion H0 ; auto. inversion H13 ; subst ; intuition.
+      inversion H0 ; auto. inversion H11 ; subst ; intuition.
+      inversion H5 ; auto. inversion H13 ; subst ; intuition.
+      rewrite <- H16 in *. rewrite <- H16 in *. intuition.
+      rewrite <- H16 in *.
+      rewrite H12 in H7. rewrite Nat.add_comm in H7. simpl in H7.
+      rewrite Nat.add_comm in H7. simpl in H7.
+      clear H12 H9.
+      induction (distance_i (construct_checker_input (parent_i (construct_checker_input x1)))).
+      inversion H7. inversion H7. intuition.
+      assert (Graph v a). apply (Connected_Isa_Graph v a c0) ; auto.
+      apply (G_ina_inv1 v ) in H13 ; auto. intuition.
+      apply (lsl x1 y) ; auto. apply In_right. auto. apply In_right. auto.
+      apply In_right. auto.
+      
+      
+
+      
+    + (* assert (H0' := H0). assert (H0'' := H0). *)
       rewrite (lsl x x0) ; auto.
       rewrite (lsl x x0) in H ; auto.
       clear Hx x. rename x0 into x.
+
+      apply H ; intuition.
+      assert (H0' := H0). assert (H0'' := H0). (* assert (H0''' := H0). *) clear H.
+      specialize (H0 x). specialize (H0' y). specialize (H0'' x0).
+      intuition ; subst ; intuition ; right ; intuition.
+      rewrite (lsl x y) in * ; auto. subst.
+      symmetry in H2. intuition.
+      inversion H7 ; auto. inversion H8 ; subst ; intuition.
+      rewrite H3 in *. clear H3 H9. simpl in *.
+      rewrite (lsl (parent_i (construct_checker_input x0)) x0) in * ; auto.
+      rewrite <- H in *. clear H. clear H2 H5 H4 H8.
+      
+
+      destruct (V_eq_dec y (parent_i (construct_checker_input x))) ; subst ; intuition.
+      admit.
+      specialize (H0 x).
+      destruct (V_eq_dec x (parent_i (construct_checker_input y))) ; subst.
+      rewrite (lsl (parent_i (construct_checker_input y)) y) in * ; auto.
+      apply H ; auto. clear H. intuition.
+      specialize (H0 x) ; intuition. right. intuition.
+      
+      admit.
+
+
+      apply H ; intuition.
+      apply (H0 x0) ; auto. clear H.
+      specialize (H0 x0) ; intuition.
+      right. intuition.
+      inversion H.
+      inversion H3 ; subst ; intuition.
+      auto.
+
+
+
 
       specialize (H0 x).
       intuition ; subst ; intuition.
